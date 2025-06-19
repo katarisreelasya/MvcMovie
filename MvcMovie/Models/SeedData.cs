6f94458ab956a48f1fa4ffb18da910e056035eb2
sreelasya@lonrix.in <sreelasya@lonrix.in>
2025-06-19 11:04:00 +0530

    rating

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -17,7 +17,7 @@ public static class SeedData
             // Look for any movies.
             if (context.Movie.Any())
             {
-                return;   // DB has been seeded
+                return;   
             }
             context.Movie.AddRange(
                 new Movie
@@ -25,29 +25,10 @@ public static class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating="9",
                     Price = 7.99M
-                },
-                new Movie
-                {
-                    Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                    Genre = "Comedy",
-                    Price = 8.99M
-                },
-                new Movie
-                {
-                    Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                    Genre = "Comedy",
-                    Price = 9.99M
-                },
-                new Movie
-                {
-                    Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                    Genre = "Western",
-                    Price = 3.99M
                 }
+                
             );
             context.SaveChanges();
         }
